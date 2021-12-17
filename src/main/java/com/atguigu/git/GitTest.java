e54a05b24d0853e1241f1889f3bb497be8c7be17
cjy11395 <1607885070@qq.com>
2021-12-17 17:38:12 +0800

    cjy git-test commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @author CJY
+ * @date 2021/12/17 - 11:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("master test!");
+        System.out.println("hot-fix test!");
+    }
+}
